cba0413b37f53ebf53902cb01ffb4bd198e8ebb2
dxnale <daniel.torrealba@alumnos.ipleones.cl>
2025-11-26 18:41:36 +0100

    Test: Routing changes to include repo name

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,15 @@
 import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
+  const baseUrl = import.meta.env.BASE_URL;
+  let path = url.pathname;
+  if (path.startsWith(baseUrl)) {
+    path = path.slice(baseUrl.length);
+  }
+  if (path.startsWith('/')) {
+    path = path.slice(1);
+  }
+  const [lang] = path.split('/');
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
